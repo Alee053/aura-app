c9751bf51e39112c84b0aec3c5012a2a278749dd
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-04-12 21:19:27 -0400

    fix: register TodoViewModel in TodoModule
    The TodoModule only registered TodoRepository, causing Koin to fail at
    runtime when attempting to inject TodoViewModel.

--- composeApp/src/commonMain/kotlin/com/programovil/aura/todo/di/TodoModule.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/todo/di/TodoModule.kt
@@ -2,8 +2,11 @@ package com.programovil.aura.todo.di
 
 import com.programovil.aura.todo.data.repository.TodoRepositoryImpl
 import com.programovil.aura.todo.domain.repository.TodoRepository
+import com.programovil.aura.todo.presentation.TodoViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val todoModule = module {
     single<TodoRepository> { TodoRepositoryImpl() }
+    viewModel { TodoViewModel(get()) }
 }

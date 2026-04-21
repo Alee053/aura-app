fcceb4d3ee694c5b0e1a88201e5fc3d49761f6aa
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-04-21 17:20:05 -0400

    feat: add dueDate field to Todo model

--- composeApp/src/commonMain/kotlin/com/programovil/aura/todo/domain/model/Todo.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/todo/domain/model/Todo.kt
@@ -3,5 +3,6 @@ package com.programovil.aura.todo.domain.model
 data class Todo(
     val id: String,
     val title: String,
-    val isCompleted: Boolean = false
+    val isCompleted: Boolean = false,
+    val dueDate: Long? = null  // epoch millis, nullable
 )

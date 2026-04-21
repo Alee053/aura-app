606dd2515d4a66d473afe81977bb100f94466f71
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-04-21 17:22:15 -0400

    feat(habit): add HabitRepository interface

---
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/habit/domain/repository/HabitRepository.kt
@@ -0,0 +1,14 @@
+package com.programovil.aura.habit.domain.repository
+
+import com.programovil.aura.habit.domain.model.Habit
+import com.programovil.aura.habit.domain.model.HabitCompletion
+import kotlinx.coroutines.flow.Flow
+
+interface HabitRepository {
+    fun getHabits(): Flow<List<Habit>>
+    fun getCompletionsForHabit(habitId: String): Flow<List<HabitCompletion>>
+    suspend fun addHabit(habit: Habit): Result<Unit>
+    suspend fun deleteHabit(habitId: String): Result<Unit>
+    suspend fun toggleCompletion(habitId: String, date: String): Result<Unit>
+    suspend fun cleanupOldCompletions(olderThanDays: Int): Result<Unit>
+}

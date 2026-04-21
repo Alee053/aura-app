0b378b5ad3d1af3029862b11957a67a41726ab4b
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-04-21 17:42:19 -0400

    feat(habit): add HabitModule DI and register in InitKoin

---
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/habit/di/HabitModule.kt
@@ -0,0 +1,28 @@
+package com.programovil.aura.habit.di
+
+import com.programovil.aura.habit.data.repository.HabitRepositoryImpl
+import com.programovil.aura.habit.domain.repository.HabitRepository
+import com.programovil.aura.habit.domain.usecase.AddHabitUseCase
+import com.programovil.aura.habit.domain.usecase.GetHabitsGroupedByDayUseCase
+import com.programovil.aura.habit.domain.usecase.GetHabitHistoryUseCase
+import com.programovil.aura.habit.domain.usecase.ToggleHabitCompletionUseCase
+import com.programovil.aura.habit.presentation.viewmodel.HabitViewModel
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val habitModule = module {
+    // Data layer
+    singleOf(::HabitRepositoryImpl).bind<HabitRepository>()
+
+    // Domain layer - use cases
+    factoryOf(::AddHabitUseCase)
+    factoryOf(::GetHabitsGroupedByDayUseCase)
+    factoryOf(::ToggleHabitCompletionUseCase)
+    factoryOf(::GetHabitHistoryUseCase)
+
+    // Presentation layer
+    viewModel { HabitViewModel(get(), get(), get(), get()) }
+}

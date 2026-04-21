4796ad7199165a6ff3de0e86eb994d3f74f55246
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-04-21 17:26:57 -0400

    fix(habit): add getAllCompletions method to repository interface

--- composeApp/src/commonMain/kotlin/com/programovil/aura/habit/domain/repository/HabitRepository.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/habit/domain/repository/HabitRepository.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.Flow
 interface HabitRepository {
     fun getHabits(): Flow<List<Habit>>
     fun getCompletionsForHabit(habitId: String): Flow<List<HabitCompletion>>
+    fun getAllCompletions(): Flow<List<HabitCompletion>>
     suspend fun addHabit(habit: Habit): Result<Unit>
     suspend fun deleteHabit(habitId: String): Result<Unit>
     suspend fun toggleCompletion(habitId: String, date: String): Result<Unit>

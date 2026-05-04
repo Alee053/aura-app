0dc0f29af8a309c40b6d42f7f96a5c9687673314
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-04 19:52:17 -0400

    refactor(habit): replace Room DB DI with createHabitRepository factory

--- composeApp/src/commonMain/kotlin/com/programovil/aura/habit/di/HabitModule.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/habit/di/HabitModule.kt
@@ -1,25 +1,19 @@
 package com.programovil.aura.habit.di
 
-import com.programovil.aura.habit.data.local.getHabitDatabase
-import com.programovil.aura.habit.data.local.getHabitDatabaseBuilder
-import com.programovil.aura.habit.data.repository.HabitRepositoryImpl
 import com.programovil.aura.habit.domain.repository.HabitRepository
+import com.programovil.aura.habit.domain.repository.createHabitRepository
 import com.programovil.aura.habit.domain.usecase.AddHabitUseCase
 import com.programovil.aura.habit.domain.usecase.GetHabitHistoryUseCase
 import com.programovil.aura.habit.domain.usecase.GetHabitsGroupedByDayUseCase
 import com.programovil.aura.habit.domain.usecase.ToggleHabitCompletionUseCase
 import com.programovil.aura.habit.presentation.viewmodel.HabitViewModel
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val habitModule = module {
     // Data layer
-    single { getHabitDatabaseBuilder() }
-    single { getHabitDatabase(get()) }
-    singleOf(::HabitRepositoryImpl).bind<HabitRepository>()
+    single { createHabitRepository() }
 
     // Domain layer - use cases
     factoryOf(::AddHabitUseCase)

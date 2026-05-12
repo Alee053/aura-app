d4613d5e9d21c6414f89b12166d658c3069ad04e
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-12 00:42:34 -0400

    Update AppNavHost with Home, Tasks, Focus, Progress, Settings routes

--- composeApp/src/commonMain/kotlin/com/programovil/aura/navigation/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/navigation/AppNavHost.kt
@@ -5,20 +5,69 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import com.programovil.aura.designsystem.theme.ThemeMode
 import com.programovil.aura.habit.presentation.screen.HabitScreen
-import com.programovil.aura.notification.presentation.screen.NotificationSettingsScreen
-import com.programovil.aura.notification.presentation.viewmodel.NotificationViewModel
+import com.programovil.aura.home.presentation.screen.FocusScreen
+import com.programovil.aura.home.presentation.screen.HomeScreen
+import com.programovil.aura.home.presentation.screen.ProgressScreen
+import com.programovil.aura.home.presentation.screen.SettingsScreen
+import com.programovil.aura.home.presentation.screen.TasksScreen
+import com.programovil.aura.home.presentation.viewmodel.HomeViewModel
+import com.programovil.aura.home.presentation.viewmodel.FocusViewModel
+import com.programovil.aura.home.presentation.viewmodel.ProgressViewModel
+import com.programovil.aura.home.presentation.viewmodel.SettingsViewModel
 import com.programovil.aura.todo.presentation.screen.TodoScreen
 import com.programovil.aura.todo.presentation.viewmodel.TodoViewModel
-import org.koin.compose.koinInject
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun AppNavHost(
     navController: NavHostController,
     todoViewModel: TodoViewModel,
+    currentThemeMode: ThemeMode,
+    onThemeChange: (ThemeMode) -> Unit,
     onSignOut: () -> Unit
 ) {
-    NavHost(navController = navController, startDestination = NavRoute.Todo) {
+    NavHost(navController = navController, startDestination = NavRoute.Home) {
+        composable<NavRoute.Home> {
+            val homeViewModel = koinViewModel<HomeViewModel>()
+            HomeScreen(
+                viewModel = homeViewModel,
+                onTasksClick = { navController.navigate(NavRoute.Tasks) },
+                onFocusClick = { navController.navigate(NavRoute.Focus) },
+                onProgressClick = { navController.navigate(NavRoute.Progress) },
+                onSettingsClick = { navController.navigate(NavRoute.Settings) }
+            )
+        }
+        composable<NavRoute.Tasks> {
+            TasksScreen(
+                todoViewModel = todoViewModel,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+        composable<NavRoute.Focus> {
+            val focusViewModel = koinViewModel<FocusViewModel>()
+            FocusScreen(
+                viewModel = focusViewModel,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+        composable<NavRoute.Progress> {
+            val progressViewModel = koinViewModel<ProgressViewModel>()
+            ProgressScreen(
+                viewModel = progressViewModel,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+        composable<NavRoute.Settings> {
+            val settingsViewModel = koinViewModel<SettingsViewModel>()
+            SettingsScreen(
+                viewModel = settingsViewModel,
+                currentThemeMode = currentThemeMode,
+                onThemeChange = onThemeChange,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
         composable<NavRoute.Todo> {
             TodoScreen(
                 viewModel = todoViewModel,
@@ -32,13 +81,6 @@ fun AppNavHost(
         composable<NavRoute.Habit> {
             HabitScreen(
                 onSignOut = onSignOut
-            )
-        }
-        composable<NavRoute.NotificationSettings> {
-            val notificationViewModel: NotificationViewModel = koinInject()
-            NotificationSettingsScreen(
-                viewModel = notificationViewModel,
-                onBackClick = { navController.popBackStack() }
             )
         }
     }

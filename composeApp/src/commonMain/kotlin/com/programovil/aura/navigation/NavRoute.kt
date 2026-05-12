8121186536514dc5365181d9ac460385ecd65131
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-12 00:42:24 -0400

    Update NavRoute.kt: replace NotificationSettings with Home, Tasks, Focus, Progress, Settings routes

--- composeApp/src/commonMain/kotlin/com/programovil/aura/navigation/NavRoute.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/navigation/NavRoute.kt
@@ -5,10 +5,22 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed class NavRoute {
     @Serializable
-    data object Todo : NavRoute()
+    data object Home : NavRoute()
 
     @Serializable
-    data object NotificationSettings : NavRoute()
+    data object Tasks : NavRoute()
+
+    @Serializable
+    data object Focus : NavRoute()
+
+    @Serializable
+    data object Progress : NavRoute()
+
+    @Serializable
+    data object Settings : NavRoute()
+
+    @Serializable
+    data object Todo : NavRoute()
 
     @Serializable
     data class TodoDetail(val todoId: String) : NavRoute()

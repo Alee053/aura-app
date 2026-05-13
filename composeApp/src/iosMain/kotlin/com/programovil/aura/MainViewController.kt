0df7c7f9b2811454fa2f4b25bcb64ef7af76a00d
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-13 19:41:10 -0400

    fix(ios): initialize Koin in MainViewController to prevent runtime crash

--- composeApp/src/iosMain/kotlin/com/programovil/aura/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/programovil/aura/MainViewController.kt
@@ -1,5 +1,13 @@
 package com.programovil.aura
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.programovil.aura.di.getModules
+import com.programovil.aura.shared.StubRemoteConfigService
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    startKoin {
+        modules(getModules(StubRemoteConfigService()))
+    }
+    App()
+}

9c6ffee3439c73d23bcb402bc18aae060dfb1da1
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-13 19:42:09 -0400

    fix(ios): use lazy Koin init to avoid repeated initialization

--- composeApp/src/iosMain/kotlin/com/programovil/aura/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/programovil/aura/MainViewController.kt
@@ -5,9 +5,13 @@ import com.programovil.aura.di.getModules
 import com.programovil.aura.shared.StubRemoteConfigService
 import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController {
+private val koinApp by lazy {
     startKoin {
         modules(getModules(StubRemoteConfigService()))
     }
+}
+
+fun MainViewController() = ComposeUIViewController {
+    koinApp
     App()
 }

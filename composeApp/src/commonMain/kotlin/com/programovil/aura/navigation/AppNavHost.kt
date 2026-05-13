34054eba3d163159d83fdbff198e435f9faf4117
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-13 18:16:59 -0400

    refactor(nav): simplify AppNavHost to 4 routes

--- composeApp/src/commonMain/kotlin/com/programovil/aura/navigation/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/navigation/AppNavHost.kt
@@ -4,17 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
 import com.programovil.aura.designsystem.theme.ThemeMode
 import com.programovil.aura.habit.presentation.screen.HabitScreen
-import com.programovil.aura.home.presentation.screen.FocusScreen
 import com.programovil.aura.home.presentation.screen.HomeScreen
-import com.programovil.aura.home.presentation.screen.ProgressScreen
-import com.programovil.aura.settings.presentation.screen.SettingsScreen
-import com.programovil.aura.home.presentation.screen.TasksScreen
 import com.programovil.aura.home.presentation.viewmodel.HomeViewModel
-import com.programovil.aura.home.presentation.viewmodel.FocusViewModel
-import com.programovil.aura.home.presentation.viewmodel.ProgressViewModel
+import com.programovil.aura.settings.presentation.screen.SettingsScreen
 import com.programovil.aura.settings.presentation.viewmodel.SettingsViewModel
 import com.programovil.aura.todo.presentation.screen.TodoScreen
 import com.programovil.aura.todo.presentation.viewmodel.TodoViewModel
@@ -25,62 +19,32 @@ fun AppNavHost(
     navController: NavHostController,
     todoViewModel: TodoViewModel,
     currentThemeMode: ThemeMode,
-    onThemeChange: (ThemeMode) -> Unit,
-    onSignOut: () -> Unit
+    onThemeChange: (ThemeMode) -> Unit
 ) {
     NavHost(navController = navController, startDestination = NavRoute.Home) {
         composable<NavRoute.Home> {
             val homeViewModel = koinViewModel<HomeViewModel>()
             HomeScreen(
                 viewModel = homeViewModel,
-                onTasksClick = { navController.navigate(NavRoute.Tasks) },
-                onFocusClick = { navController.navigate(NavRoute.Focus) },
-                onProgressClick = { navController.navigate(NavRoute.Progress) },
+                onTodoClick = { navController.navigate(NavRoute.Todo) },
+                onHabitClick = { navController.navigate(NavRoute.Habit) },
                 onSettingsClick = { navController.navigate(NavRoute.Settings) }
             )
         }
-        composable<NavRoute.Tasks> {
-            TasksScreen(
-                todoViewModel = todoViewModel,
-                onBackClick = { navController.popBackStack() }
+        composable<NavRoute.Todo> {
+            TodoScreen(
+                viewModel = todoViewModel
             )
         }
-        composable<NavRoute.Focus> {
-            val focusViewModel = koinViewModel<FocusViewModel>()
-            FocusScreen(
-                viewModel = focusViewModel,
-                onBackClick = { navController.popBackStack() }
-            )
-        }
-        composable<NavRoute.Progress> {
-            val progressViewModel = koinViewModel<ProgressViewModel>()
-            ProgressScreen(
-                viewModel = progressViewModel,
-                onBackClick = { navController.popBackStack() }
-            )
+        composable<NavRoute.Habit> {
+            HabitScreen()
         }
         composable<NavRoute.Settings> {
             val settingsViewModel = koinViewModel<SettingsViewModel>()
             SettingsScreen(
                 viewModel = settingsViewModel,
                 currentThemeMode = currentThemeMode,
-                onThemeChange = onThemeChange,
-                onBackClick = { navController.popBackStack() }
-            )
-        }
-        composable<NavRoute.Todo> {
-            TodoScreen(
-                viewModel = todoViewModel,
-                onSignOut = onSignOut
-            )
-        }
-        composable<NavRoute.TodoDetail> { backStackEntry ->
-            val todoDetail: NavRoute.TodoDetail = backStackEntry.toRoute()
-            // TODO: Navigate to detail screen with todoDetail.todoId
-        }
-        composable<NavRoute.Habit> {
-            HabitScreen(
-                onSignOut = onSignOut
+                onThemeChange = onThemeChange
             )
         }
     }

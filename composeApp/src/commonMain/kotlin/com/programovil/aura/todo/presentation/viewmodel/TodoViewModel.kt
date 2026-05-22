ba59b27e6f1d511535dc22d74cfd87e1f1b8a2b7
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-21 21:26:17 -0400

    feat(todo): add updateTodo to ViewModel and register UpdateTodoUseCase in DI

--- composeApp/src/commonMain/kotlin/com/programovil/aura/todo/presentation/viewmodel/TodoViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/todo/presentation/viewmodel/TodoViewModel.kt
@@ -7,6 +7,7 @@ import com.programovil.aura.todo.domain.usecase.AddTodoUseCase
 import com.programovil.aura.todo.domain.usecase.DeleteTodoUseCase
 import com.programovil.aura.todo.domain.usecase.GetTodosUseCase
 import com.programovil.aura.todo.domain.usecase.ToggleTodoUseCase
+import com.programovil.aura.todo.domain.usecase.UpdateTodoUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -14,6 +15,7 @@ import kotlinx.coroutines.launch
 class TodoViewModel(
     private val getTodosUseCase: GetTodosUseCase,
     private val addTodoUseCase: AddTodoUseCase,
+    private val updateTodoUseCase: UpdateTodoUseCase,
     private val toggleTodoUseCase: ToggleTodoUseCase,
     private val deleteTodoUseCase: DeleteTodoUseCase
 ) : ViewModel() {
@@ -62,6 +64,14 @@ class TodoViewModel(
         viewModelScope.launch {
             deleteTodoUseCase(todoId)
                 .onFailure { _error.value = "Failed to delete todo" }
+        }
+    }
+
+    fun updateTodo(todo: Todo) {
+        _error.value = null
+        viewModelScope.launch {
+            updateTodoUseCase(todo)
+                .onFailure { _error.value = "Failed to update todo" }
         }
     }
 

81d0281d60df3774e9c65fa71f6d83b65478ec3f
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-21 21:18:04 -0400

    feat(todo): add optional description field to Todo model and mapper

--- composeApp/src/commonMain/kotlin/com/programovil/aura/todo/domain/model/Todo.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/todo/domain/model/Todo.kt
@@ -3,6 +3,7 @@ package com.programovil.aura.todo.domain.model
 data class Todo(
     val id: String,
     val title: String,
+    val description: String? = null,
     val isCompleted: Boolean = false,
-    val dueDate: Long? = null  // epoch millis, nullable
+    val dueDate: Long? = null
 )

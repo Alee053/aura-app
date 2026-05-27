d2d1da386c9fbde912b24e7bc11c06af7b79cbf9
Alejandro Castro <alejandro.castro.0520@gmail.com>
2026-05-27 18:34:45 -0400

    fix(onboarding): add OnboardingViewModel to DI module

--- composeApp/src/commonMain/kotlin/com/programovil/aura/onboarding/di/OnboardingModule.kt
+++ composeApp/src/commonMain/kotlin/com/programovil/aura/onboarding/di/OnboardingModule.kt
@@ -5,8 +5,10 @@ import com.programovil.aura.onboarding.data.OnboardingPreferencesImpl
 import com.programovil.aura.onboarding.data.repository.OnboardingRepositoryImpl
 import com.programovil.aura.onboarding.domain.repository.OnboardingRepository
 import com.programovil.aura.onboarding.domain.usecase.GetOnboardingSlidesUseCase
+import com.programovil.aura.onboarding.presentation.OnboardingViewModel
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
@@ -14,4 +16,5 @@ val onboardingModule = module {
     singleOf(::OnboardingRepositoryImpl) bind OnboardingRepository::class
     singleOf(::OnboardingPreferencesImpl) bind OnboardingPreferences::class
     factoryOf(::GetOnboardingSlidesUseCase)
+    viewModelOf(::OnboardingViewModel)
 }

5359dab929a1f7ebfcc8b944ebf416cad3efea9e
sebas123312231 <arceantezanasebastianalejandro@gmail.com>
2026-05-27 18:54:49 -0400

    fix avance 2

--- composeApp/src/commonTest/kotlin/com/programovil/aura/shared/FeatureFlagManagerTest.kt
+++ composeApp/src/commonTest/kotlin/com/programovil/aura/shared/FeatureFlagManagerTest.kt
@@ -61,6 +61,7 @@ private class FakeRemoteConfigService(
 ) : RemoteConfigService {
     override suspend fun getBoolean(flag: FeatureFlag): Boolean = booleanValues[flag] ?: flag.defaultValue
     override suspend fun getString(flag: FeatureFlag, default: String): String = default
+    override suspend fun getString(key: String): String? = null
     override suspend fun fetchAndActivate(): Result<Unit> = fetchResult
     override fun registerOnConfigUpdateListener(onUpdate: () -> Unit) {}
 }
